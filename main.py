dc74b971d0a7331d7191a1f98c245dbb7e9e1d9a
Diana <pdsanaa@gmail.com>
2019-05-30 19:32:36 +0300

    calculate with input

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 from ast import *
+
+def main():
+    while True:
+        try:
+            try:
+                text = raw_input('spi> ')
+            except NameError:  # Python3
+                text = input('spi> ')
+        except EOFError:
+            break
+        if not text:
+            continue
+
+        lexer = Lexer(text)
+        parser = Parser(lexer)
+        interpreter = Interpreter(parser)
+        result = interpreter.interpret()
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
