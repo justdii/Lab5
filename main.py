527de0e39f8810de3cc83090120f5c0b76a2cb78
coden256 <den.blackshov@gmail.com>
2019-06-03 19:41:14 +0300

    now interpreting from file

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
 from ast import *
 
 def main():
-    text = '''
-    a := 5*99999+0;
-    b:= 35 + 3            -- 1 * a;
-    print b;
-    print a;
-    c:= 99*9 + a -b;
-    print c + 2*10*(1-2)
-    '''
 
-    iftext = '''
-    a := 3;
-    c := 5;
-    if a-2 {
-        x := 16;
-        if x*2 {
-            print 0
-        };
-        print 8;
-        print 17
-    }else {
-        print 4;
-        print c
-    };
-    print 99
-    '''
-
-    lexer = Lexer(iftext)
-
+    with open("source.dd") as f:
+        code = ''.join(f.readlines())
+    print(code)
+    lexer = Lexer(code)
     parser = Parser(lexer)
     interpreter = Interpreter(parser)
     result = interpreter.interpret()
